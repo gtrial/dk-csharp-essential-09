a1d474c8697f1891ca143752c8dfd8941daa2bd6
gtrial <gtrial@gmail.com>
2021-04-30 19:29:10 +0300

    Task 02 completed

--- task02/Program.cs
+++ task02/Program.cs
@@ -4,12 +4,39 @@ namespace task02
 {
     internal class Program
     {
+        public delegate double Add(double x, double y);
+        public delegate double Sub(double x, double y);
+        public delegate double Mul(double x, double y);
+        public delegate double Div(double x, double y);
+
         private static void Main()
         {
-            //Создайте четыре лямбда оператора для выполнения арифметических действий: (Add – сложение, Sub – вычитание, Mul – умножение, Div – деление).
-            //Каждый лямбда оператор должен принимать два аргумента и возвращать результат вычисления.
-            //Лямбда оператор деления должен делать проверку деления на ноль.
-            //Написать программу, которая будет выполнять арифметические действия, указанные пользователем.
+            double x = 1.4;
+            double y = 2.5;
+            Console.WriteLine("Operation:");
+            string operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "Add":
+                    Add add = delegate (double x, double y) { return x + y; };
+                    Console.WriteLine($"{operation} result={add(x, y)}");
+                    break;
+                case "Sub":
+                    Sub sub = delegate (double x, double y) { return x - y; };
+                    Console.WriteLine($"{operation} result={sub(x, y)}");
+                    break;
+                case "Mul":
+                    Mul mul = delegate (double x, double y) { return x * y; };
+                    Console.WriteLine($"{operation} result={mul(x, y)}");
+                    break;
+                case "Div":
+                    Div div = delegate (double x, double y) { if (y == 0) { Console.WriteLine("Division by 0"); return y; } return x / y; };
+                    Console.WriteLine($"{operation} result={div(x, y)}");
+                    break;
+                default:
+                    Console.WriteLine($"Unsupported operation {operation}");
+                    break;
+            }
         }
     }
 }
