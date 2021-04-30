d8468c2902faae4151dbe16ae851d6302b1d79de
gtrial <gtrial@gmail.com>
2021-04-30 21:09:31 +0300

    Task 03 completed

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,12 +1,43 @@
-﻿namespace task03
+﻿using System;
+
+namespace task03
 {
     internal class Program
     {
+        public delegate double MyDelegate(Delegate[] delegates);
+        public delegate int Delegate();
+
         private static void Main()
         {
-            //Создайте анонимный метод, который принимает в качестве аргумента массив делегатов
-            //и возвращает среднее арифметическое возвращаемых значений методов, сообщенных с делегатами в массиве.
-            //Методы, сообщенные с делегатами из массива, возвращают случайное значение типа int.
+            Random random = new Random();
+            Delegate[] delegates = new Delegate[] {
+                delegate {
+                    var result = random.Next(1, 10);
+                    Console.WriteLine($"{result}");
+                    return result; },
+                delegate {
+                    var result = random.Next(1, 10);
+                    Console.WriteLine($"{result}");
+                    return result;
+                } };
+            MyDelegate myDelegate = delegate
+            {
+                double result = 0;
+                for (int i = 0; i < delegates.Length; i++)
+                {
+                    result += delegates[i]();
+                }
+                if (delegates.Length > 0)
+                {
+                    return (double)result / delegates.Length;
+                }
+                else
+                {
+                    return 0;
+                }
+            }; ;
+            Console.WriteLine($"{myDelegate(delegates)}");
         }
     }
 }
+
